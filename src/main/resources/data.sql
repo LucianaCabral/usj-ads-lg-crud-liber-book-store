ad8770c49e9f283be2e3c7998a5996fcc2c79ecf
Luciana <eng.luciana.cabral@gmail.com>
2020-12-02 22:43:50 -0300

    pom

--- src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-insert into livro (id, autor, categoria, isbn, titulo) values (1, 'Fagundes Varela', 'Drama', 'B123456789N', 'Notas do Subsolo');
-insert into livro (id, autor, categoria, isbn, titulo) values (2, 'Bruce Dickinson', 'Biografia', 'U123456789T', 'Bruce Dickinson');
-insert into livro (id, autor, categoria, isbn, titulo) values (3, 'Julian Assange', 'Biografia', 'U123456789T', 'Wikileaks');
-
-insert into cliente (id, nome, cpf, telefone) values (1, 'Armenidas Souza', '910101101', '9925999999');
-insert into cliente (id, nome, cpf, telefone) values (2, 'Arbertina Alves', '910345345', '9925444444');
-insert into cliente (id, nome, cpf, telefone) values (3, 'Almerinda Casto', '910101101', '9925999999');
